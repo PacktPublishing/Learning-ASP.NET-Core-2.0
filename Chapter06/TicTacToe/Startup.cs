1ccc9bd1379a2615cad4f7ab36c5ef5fa80a2b98
Abhishek1373 <abhisheksharma@packtpub.com>
2017-12-11 15:40:11 +0530

    Code files added

---
+++ Chapter06/TicTacToe/Startup.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using TicTacToe.Services;
+using TicTacToe.Extensions;
+using Microsoft.AspNetCore.Routing;
+using TicTacToe.Models;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using TicTacToe.Options;
+using TicTacToe.Filters;
+using TicTacToe.ViewEngines;
+
+namespace TicTacToe
+{
+    public class Startup
+    {
+        public IConfiguration _configuration { get; }
+        public IHostingEnvironment _hostingEnvironment { get; }
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            _configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public void ConfigureCommonServices(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Localization");
+            services.AddMvc(o => o.Filters.Add(typeof(DetectMobileFilter))).AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, options => options.ResourcesPath = "Localization").AddDataAnnotationsLocalization();
+
+            services.AddSingleton<IUserService, UserService>();
+            services.AddSingleton<IGameInvitationService, GameInvitationService>();
+            services.AddSingleton<IGameSessionService, GameSessionService>();
+
+            services.Configure<EmailServiceOptions>(_configuration.GetSection("Email"));
+            services.AddEmailService(_hostingEnvironment, _configuration);
+            services.AddTransient<IEmailTemplateRenderService, EmailTemplateRenderService>();
+            services.AddTransient<EmailViewEngine, EmailViewEngine>();
+
+            services.AddRouting();
+            services.AddSession(o =>
+            {
+                o.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            ConfigureCommonServices(services);
+        }
+
+        public void ConfigureStagingServices(IServiceCollection services)
+        {
+            ConfigureCommonServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            ConfigureCommonServices(services);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("CreateUser", context =>
+            {
+                var firstName = context.Request.Query["firstName"];
+                var lastName = context.Request.Query["lastName"];
+                var email = context.Request.Query["email"];
+                var password = context.Request.Query["password"];
+                var userService = context.RequestServices.GetService<IUserService>();
+                userService.RegisterUser(new UserModel { FirstName = firstName, LastName = lastName, Email = email, Password = password });
+                return context.Response.WriteAsync($"User {firstName} {lastName} has been sucessfully created.");
+            });
+            var newUserRoutes = routeBuilder.Build();
+            app.UseRouter(newUserRoutes);
+
+            app.UseWebSockets();
+            app.UseCommunicationMiddleware();
+
+            var supportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            localizationOptions.RequestCultureProviders.Clear();
+            localizationOptions.RequestCultureProviders.Add(new CultureProviderResolverService());
+
+            app.UseRequestLocalization(localizationOptions);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "areaRoute",
+                        template: "{area:exists}/{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseStatusCodePages("text/plain", "HTTP Error - Status Code: {0}");
+        }
+    }
+}
---
+++ Chapter07/TicTacToe/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TicTacToe.Models;
+
+namespace TicTacToe.Services
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterUser(UserModel userModel);
+        Task<UserModel> GetUserByEmail(string email);
+        Task UpdateUser(UserModel user);
+        Task<IEnumerable<UserModel>> GetTopUsers(int numberOfUsers);
+    }
+}
